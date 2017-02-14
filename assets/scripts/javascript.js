e7884ff486cee36a0737548a0b8db86ab5f36f27
Daniel Segovia <daniel@dabici.mx>
2017-02-13 21:51:59 -0600

    disabled buttons when sequence is executing, added the stric mode. finished

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -5,37 +5,49 @@ $(document).ready(function() {
 	}
 	var sequence = [randomize()];
 
-	//sequence = [0,1,2,3,3,2,1,0]; For test purposes.
+	 //For test purposes.
 	console.log(sequence);
 
 	function emulateSequence(){
+		console.log(sequence);
+		$(".simon-button").attr('disabled', 'disabled');
 		var i = 0, howManyTimes = sequence.length;
 		function f(){
+			audios[sequence[i]].play();
 			$("#" + sequence[i]).addClass('active').delay(1000).queue(function(){
 				$(this).removeClass('active').dequeue();
 			});
 			i++;
 			if(i < howManyTimes){
 				setTimeout( f, 2000);
+			}else{
+				$(".simon-button").removeAttr('disabled');
 			}
 		}
 		f();
 	}
 
+	var strictMode = false;
+
 	function showError(){
-		$("#round").addClass('shake');
-		$("#round").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-			$("#round").removeClass('shake');
-		});
-		setTimeout(emulateSequence, 2000);
+		if (strictMode) {
+			resetGame();
+			setTimeout(emulateSequence, 2000);
+		}else{
+			$("#round").addClass('shake');
+			$("#round").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$("#round").removeClass('shake');
+			});
+			setTimeout(emulateSequence, 2000);
+		}
 	}
 
 	function completedSequence(){
-		console.log("Finished the sequence! Adding 1 to the sequence.")
 		if (sequence.length >= 20) {
 			resetGame();
 			console.log("finished the game")
 		} else {
+			console.log("Finished the sequence! Adding 1 to the sequence.")
 			sequence.push(randomize());
 			window.setTimeout(emulateSequence, 2000);
 			$("#round").text("Round: " + sequence.length).addClass('bounce');
@@ -51,6 +63,7 @@ $(document).ready(function() {
 		$("#round").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
 				$("#round").removeClass('flash');
 			});
+		$(".btn-strict").removeAttr('disabled');
 	}
 
 	var audios = [new Audio('assets/sounds/simonSound0.mp3'), 
@@ -60,6 +73,7 @@ $(document).ready(function() {
 
 	var index = 0;
 	$(".simon-button").click(function(event) {
+		$(".btn-strict").attr('disabled', 'disabled');
 		audios[this.id].play();
 		this.blur();
 		if (this.id == sequence[index]) {  	//what to do if the button is the same as the sequence
@@ -89,5 +103,22 @@ $(document).ready(function() {
 	$("#reset").click(function(event) {
 		resetGame();
 	});
+
+	$(".btn-strict").click(function(event) {
+		if (this.id === "strict-on") {
+			$("#strict-on").addClass('active');
+			$("#strict-off").removeClass('active');
+			strictMode = true;
+			$("#strict-mode-text").html("Strict Mode: On");
+		}else{
+			$("#strict-off").addClass('active');
+			$("#strict-on").removeClass('active');
+			strictMode = false;
+			$("#strict-mode-text").html("Strict Mode: Off");
+		}
+	});
+
+	resetGame();
 });
+
 
